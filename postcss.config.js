6185e23d4ce2b5972f058577e85815d509133793
TJ Fogarty <tj.fogarty@teamwork.com>
2020-04-21 20:57:31 +0100

    Start using tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,25 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const isDev = process.env.NODE_ENV === 'development'
+
+const plugins = [
+  require('postcss-import'),
+  require('tailwindcss'),
+  require('postcss-preset-env')({
+    browsers: 'last 2 versions'
+  })
+]
+
+if (!isDev) {
+  plugins.push(require('cssnano')({
+    preset: 'default',
+  }));
+  plugins.push(purgecss({
+    content: ['./posts/**/*.md', './_includes/**/*.*', './about/index.md', './*.njk'],
+    variables: false,
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+  }));
+}
 
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-preset-env')({
-      browsers: 'last 2 versions',
-      autoprefixer: { grid: true }
-    }),
-    purgecss({
-      content: ['./posts/**/*.md', './_includes/**/*.*', './about/index.md', './*.njk'],
-      variables: false
-    }),
-    require('cssnano')({
-      preset: 'default',
-    })
-  ]
+  plugins
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = env => {
+  const isDev = env.NODE_ENV === 'local';
+  const plugins = [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['workbox-*.*']
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: '../_includes/layouts/base.njk',
+      template: 'src/layout.njk',
+      scriptLoading: 'defer',
+      inject: false,
+    }),
+  ];
+
+  if (!isDev) {
+    plugins.push(new WorkboxPlugin.GenerateSW({
+      skipWaiting: true,
+      clientsClaim: true,
+      swDest: '../sw.js',
+      exclude: [/\.njk$/i, /\.css$/i],
+      runtimeCaching: [{
+        urlPattern: new RegExp('/$'),
+        handler: 'StaleWhileRevalidate'
+      }]
+    }))
+  }
+
   return {
-    mode: env.NODE_ENV === 'local' ? 'development' : 'production',
+    mode: isDev ? 'development' : 'production',
     entry: './src/js/index.js',
     output: {
       filename: '[name].[hash].js',
@@ -20,27 +47,6 @@ module.exports = env => {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }]
     },
-    plugins: [
-      new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ['workbox-*.*']
-      }),
-      new MiniCssExtractPlugin(),
-      new HtmlWebpackPlugin({
-        filename: '../_includes/layouts/base.njk',
-        template: 'src/layout.njk',
-        scriptLoading: 'defer',
-        inject: false,
-      }),
-      new WorkboxPlugin.GenerateSW({
-        skipWaiting: true,
-        clientsClaim: true,
-        swDest: '../sw.js',
-        exclude: [/\.njk$/i, /\.css$/i],
-        runtimeCaching: [{
-          urlPattern: new RegExp('/$'),
-          handler: 'CacheFirst'
-        }]
-      })
-    ]
+    plugins
   }
 }
