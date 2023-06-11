b6cd96b814af2c3320f7f5a33a960f47a22f49f7
TJ Fogarty <fogarty.tj@gmail.com>
2023-06-11 17:05:21 +0100

    Updates colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,41 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.njk', './src/**/*.md', './src/**/*.js'],
   theme: {
-    fontFamily: {
-      sans: ['"Source Sans 3"', 'sans-serif'],
-    },
-    container: {
-      center: true,
-      screens: {
-        'sm': '100%',
-        'md': '100%',
-        'lg': '1024px',
-        'xl': '1140px',
+    extend: {
+      fontFamily: {
+        sans: ['"Source Sans 3"', 'sans-serif'],
       },
-      padding: '2rem',
-    },
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)'
+      },
+      container: {
+        center: true,
+        screens: {
+          'sm': '100%',
+          'md': '100%',
+          'lg': '1024px',
+          'xl': '1140px',
+        },
+        padding: '2rem',
+      },
+    }
   },
   plugins: [
-    require('@tailwindcss/typography')
-  ],
+    require('@tailwindcss/typography'),
+    plugin(function({ addBase }) {
+      addBase({
+        ':root': {
+          '--color-primary': '#ff5470',
+          '--color-secondary': '#3b3c36',
+          '--color-tertiary': '#c4c3d0',
+        }
+      })
+    })
+  ]
 }
 
