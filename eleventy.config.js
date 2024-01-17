83003396abc92d4bfbccf3e661e15d2b8a9c65e4
TJ Fogarty <fogarty.tj@gmail.com>
2024-01-17 21:08:27 +0000

    Pass through apple touch icon

--- eleventy.config.js
+++ eleventy.config.js
@@ -170,6 +170,7 @@ export default async function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/site/assets");
   eleventyConfig.addPassthroughCopy("./src/site/favicon*.*");
+  eleventyConfig.addPassthroughCopy("./src/site/apple-touch-icon.png");
   eleventyConfig.addPassthroughCopy("./src/site/fonts");
   eleventyConfig.addPassthroughCopy("./src/site/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/site/robots.txt");
