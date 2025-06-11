30b2ab214918f0d5f46ea4ee67d9753291b6d97f
TJ Fogarty <fogarty.tj@gmail.com>
2025-06-11 21:01:25 +0100

    File cleanup

--- config/passthroughs.js
+++ config/passthroughs.js
@@ -5,6 +5,4 @@ export function initPassthroughs(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/site/fonts");
   eleventyConfig.addPassthroughCopy("./src/site/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/site/robots.txt");
-  eleventyConfig.addPassthroughCopy("./src/site/keybase.txt");
-  eleventyConfig.addPassthroughCopy("./src/site/googlea2c3a0ad5b2401f7.html");
 }
