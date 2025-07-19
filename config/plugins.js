36a40af07e4e6bbefa93cb49b74de62c29e860bd
TJ Fogarty <fogarty.tj@gmail.com>
2025-07-19 16:14:27 +0100

    Better image resizing

--- config/plugins.js
+++ config/plugins.js
@@ -7,5 +7,18 @@ export function initPlugins(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ["avif", "webp", "jpeg", "png"],
+    urlPath: "/assets/images/",
+    outputDir: "./_site/assets/images/",
+    widths: [500, 800],
+    htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+        sizes: "(max-width: 730px) 100vw, 730px",
+			},
+			pictureAttributes: {}
+		},
+  });
 }
--- config/shortcodes.js
+++ config/shortcodes.js
@@ -1,3 +1,40 @@
+import Image from "@11ty/eleventy-img";
+import path from "path";
+
+async function getResizedImageUrl(src) {
+  if (!src) {
+    return "";
+  }
+
+  const fullSrc = path.join("src/site/assets/images/", src);
+
+  try {
+    const metadata = await Image(fullSrc, {
+      widths: [300],
+      formats: ['auto'],
+      outputDir: "./_site/assets/images/",
+      urlPath: "/assets/images/",
+    });
+
+    const firstFormat = Object.keys(metadata)[0];
+
+    if (!firstFormat) {
+      return "";
+    }
+
+    const data = metadata[firstFormat];
+
+    if (data && data.length > 0) {
+      return data[0].url;
+    }
+  } catch (e) {
+    console.error(`::Error processing image ${fullSrc}:`, e.message);
+    return "";
+  }
+
+  return "";
+}
+
 export function initShortcodes(eleventyConfig) {
   eleventyConfig.addShortcode("glitchText", function (text, nth = 2) {
     if (!text) return '';
@@ -12,4 +49,6 @@ export function initShortcodes(eleventyConfig) {
       return word;
     }).join(' ');
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("getResizedImageUrl", getResizedImageUrl);
 }
